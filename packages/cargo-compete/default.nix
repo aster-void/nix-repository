0ae42609f450063a7f9b14fe7332b9245746bb6f
aster <137767097+aster-void@users.noreply.github.com>
2025-10-22 12:13:57 +0900

    packages: cargo-compete: add meta fields

--- packages/cargo-compete/default.nix
+++ packages/cargo-compete/default.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: let
+  inherit (pkgs) lib;
   version = "0.10.7";
   cargo-compete = pkgs.rustPlatform.buildRustPackage {
     pname = "cargo-compete";
@@ -18,6 +19,19 @@
 
     cargoHash = "sha256-lid1tyR8Y6lvjpeGJ4vGzqDTY6V2y/5rL9fGyjyF3yw=";
     doCheck = false; # this requires network access
+
+    meta = {
+      description = "A Cargo subcommand for competitive programming";
+      longDescription = ''
+        A Cargo subcommand for competitive programming.
+        Supports AtCoder, Codeforces, and yukicoder. Other websites are available via online-judge-tools/api-client.
+      '';
+      homepage = "https://github.com/qryxip/cargo-compete";
+      changelog = "https://github.com/qryxip/cargo-compete/blob/master/CHANGELOG.md";
+      license = lib.licenses.asl20;
+      maintainers = [];
+      platforms = lib.platforms.all;
+    };
   };
 in
   cargo-compete
