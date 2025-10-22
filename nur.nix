5be8c971d134d67108607094c151e1c85be46769
aster <137767097+aster-void@users.noreply.github.com>
2025-10-22 16:31:28 +0900

    meta: nur: expose some more modules

--- nur.nix
+++ nur.nix
@@ -1,3 +1,13 @@
-{pkgs ? import <nixpkgs> {}}: {
+{pkgs ? import <nixpkgs> {}}: let
+  mkNixosModule = path: {
+    _class = "nixos";
+    imports = [path];
+  };
+in {
   cargo-compete = pkgs.callPackage ./packages/cargo-compete {};
+  fcitx5-hazkey = pkgs.callPackage ./packages/fcitx5-hazkey {};
+  modules = {
+    chrome-devtools-mcp = mkNixosModule ./modules/nixos/chrome-devtools-mcp;
+    hazkey = mkNixosModule ./modules/nixos/hazkey;
+  };
 }
