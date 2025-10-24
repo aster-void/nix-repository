a6f8e1334bcc115ba8463a2d0b0f3a0e552be0fd
aster <137767097+aster-void@users.noreply.github.com>
2025-10-25 00:04:27 +0900

    dev: devshell.packages += alejandra, lefthook; dev: init lefthook

--- devshell.nix
+++ devshell.nix
@@ -1,13 +1,10 @@
 {pkgs}:
 pkgs.mkShell {
-  # Add build dependencies
-  packages = [];
-
-  # Add environment variables
-  env = {};
-
-  # Load custom bash code
+  packages = [
+    pkgs.alejandra
+    pkgs.lefthook
+  ];
   shellHook = ''
-
+    lefthook install
   '';
 }
