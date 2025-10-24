1b3e6753f008c22db9bf7f80179348de10a565b7
aster <137767097+aster-void@users.noreply.github.com>
2025-10-24 22:02:56 +0900

    nur: +ccusage +ccusage-codex +ccusage-mcp +claude-code-usage-monitor -modules.chrome-devtools-mcp

--- nur.nix
+++ nur.nix
@@ -4,12 +4,16 @@
     imports = [path];
   };
 in {
-  cargo-compete = pkgs.callPackage ./packages/cargo-compete {inherit pkgs;};
-  fcitx5-hazkey = pkgs.callPackage ./packages/fcitx5-hazkey {inherit pkgs;};
-  chrome-devtools-mcp = pkgs.callPackage ./packages/chrome-devtools-mcp {inherit pkgs;};
+  cargo-compete = import ./packages/cargo-compete {inherit pkgs;};
+  fcitx5-hazkey = import ./packages/fcitx5-hazkey {inherit pkgs;};
+  chrome-devtools-mcp = import ./packages/chrome-devtools-mcp {inherit pkgs;};
   bibata-cursors-translucent = import ./packages/bibata-cursors-translucent {inherit pkgs;};
+  ccusage = import ./packages/ccusage {inherit pkgs;};
+  ccusage-codex = import ./packages/ccusage-codex {inherit pkgs;};
+  ccusage-mcp = import ./packages/ccusage-mcp {inherit pkgs;};
+  claude-code-usage-monitor = import ./packages/claude-code-usage-monitor {inherit pkgs;};
+
   modules = {
-    chrome-devtools-mcp = mkNixosModule ./modules/nixos/chrome-devtools-mcp;
     hazkey = mkNixosModule ./modules/nixos/hazkey;
   };
 }
