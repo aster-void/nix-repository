75a07d10a2e4cf0c875fc2b8ae986278a2e217f2
aster <137767097+aster-void@users.noreply.github.com>
2025-10-28 11:09:29 +0900

    meta: nix-update: add some packages

--- update.sh
+++ update.sh
@@ -2,3 +2,7 @@
 set -euo pipefail
 
 nix-update chrome-devtools-mcp.unwrapped -f nur.nix --commit
+nix-update ccusage -f nur.nix --commit
+nix-update ccusage-codex -f nur.nix --commit
+nix-update ccusage-mcp -f nur.nix --commit
+nix-update claude-code-usage-monitor -f nur.nix --commit
