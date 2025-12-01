8160b7b2111f278869142005c916b5ed5ad001e4
aster <137767097+aster-void@users.noreply.github.com>
2025-12-01 13:37:47 +0900

    packages: helix-gj1118: update

--- packages/helix-gj1118/package.nix
+++ packages/helix-gj1118/package.nix
@@ -1,5 +1,5 @@
 {system}: let
-  rev = "aec61ce88fa0d9d0e1c6f5ebef1e222e8b97180f";
+  rev = "71072f8abd84c6b20f960ce141e31e3744c2004d";
   flake = builtins.getFlake "github:gj1118/helix/${rev}";
 in
   flake.packages.${toString system}.default
